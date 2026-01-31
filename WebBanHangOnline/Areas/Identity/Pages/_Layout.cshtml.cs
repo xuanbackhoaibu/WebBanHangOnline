a468d30453c22278c21c1ae638c76a75d8c58487
Xuân Bắc <Bxuan964@gmail.com>
2026-01-31 22:08:46 +0700

    Identity

---
+++ WebBanHangOnline/Areas/Identity/Pages/_Layout.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebBanHangOnline.Areas.Identity.Pages;
+
+public class _Layout : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
